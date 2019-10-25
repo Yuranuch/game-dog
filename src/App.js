3e1b96d9c221df1f59143ecad9957c530bc2fb9e
Yuranuch <yura477573@mail.ru>
2019-10-25 13:40:11 +0300

    add counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import photo from "./images/dog.jpg"
+import {connect} from "react-redux";
+import {imageClick, randomImage} from "./redux/reducer";
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+    componentDidMount() {
+        setInterval (()=>{
+            this.props.randomImage()
+        },1000)
+    }
 
-    </div>
-  );
+    onImageClick = () => {
+        this.props.imageClick()
+    }
+    getRandomImageIndex = ()=> {
+        return Math.floor(Math.random() * 9);
+    }
+
+    render() {
+    const photos = [
+        {id:0},
+        {id:1},
+        {id:2},
+        {id:3},
+        {id:4},
+        {id:5},
+        {id:6},
+        {id:7},
+        {id:8},
+    ]
+
+        const elements = photos.map(d => <div className="item"><img src={photo} onClick={this.onImageClick}
+            className={this.props.currentPhotoId === d.id? "photo show": "photo"}/></div>)
+
+
+        return (
+            <div>
+                <div className="wrapper">
+                    {elements}
+                </div>
+                <div className="counter">
+                    {this.props.counter}
+                </div>
+            </div>
+        );
+    }
+}
+const mapStateToProps = (state) => {
+    return {
+        currentPhotoId: state.currentPhotoId,
+        counter: state.counter
+
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        imageClick: () => {
+            dispatch(imageClick())
+        },
+        randomImage: () => {
+            dispatch(randomImage())
+        }
+
+    }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/redux/reducer.js
@@ -0,0 +1,32 @@
+export const IMAGE_CLICK = "IMAGE_CLICK"
+export const RANDOM_IMAGE = "RANDOM_IMAGE"
+
+// функция, которую если вызвать, вернёт нам случайное число от 0 до 8
+function getRandomImageIndex() {
+    return Math.floor(Math.random() * 9);
+}
+
+const initialState = {
+    currentPhotoId: 1,
+    counter: 1,
+}
+
+export const reducer = (state=initialState, action) => {
+    switch (action.type) {
+        case IMAGE_CLICK:
+            return {
+                ...state,
+                counter: state.counter +1
+            }
+        case RANDOM_IMAGE:
+            return {
+                ...state,
+                currentPhotoId: getRandomImageIndex()
+            }
+    }
+    return state
+}
+
+
+export const imageClick = () => ({type: IMAGE_CLICK})
+export const randomImage = () => ({type: RANDOM_IMAGE})
