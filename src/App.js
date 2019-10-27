bc0ee8557488fb0c5cc15bed05df69c2e5ad9af1
Yuranuch <yura477573@mail.ru>
2019-10-27 20:04:41 +0300

    align code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React, {Component} from 'react';
-import './App.css';
-import photo from "./images/dog.jpg"
-import photo_2 from "./images/dog_2.png"
+import React, {Component} from "react"
+import "./App.css"
 
 import voice from "./images/dog.mp3"
-import {connect} from "react-redux";
+import {connect} from "react-redux"
 import {
     imageClick,
     randomImage,
-    reduceTime,
     startAnimation,
     startEvents,
     stopAnimation,
@@ -17,78 +14,75 @@ import {
 
 class App extends Component {
     componentDidMount() {
-        this.audioRef=React.createRef()
-
+        this.audioRef = React.createRef()
     }
 
     onImageClick = () => {
-        this.audioRef.current.currentTime=0
+        this.audioRef.current.currentTime = 0
         this.audioRef.current.play()
         this.props.imageClick()
     }
+
     onStartEvents = () => {
-
-        // setInterval(()=>{
-        //     this.props.reduceTime()
-        // },1000)
-
-
         this.props.startEvents()
         this.props.startAnimation()
         setInterval(() => {
             if (this.props.animation) {
                 this.props.randomImage()
+                this.deadDogs()
             }
+
         }, 1000)
+
     }
     onStopEvents = () => {
         this.props.stopAnimation()
         this.props.stopEvents()
-
     }
 
-
     render() {
-    const photos = [
-        {id:0},
-        {id:1},
-        {id:2},
-        {id:3},
-        {id:4},
-        {id:5},
-        {id:6},
-        {id:7},
-        {id:8},
-    ]
+        const photos = [
+            {id: 0},
+            {id: 1},
+            {id: 2},
+            {id: 3},
+            {id: 4},
+            {id: 5},
+            {id: 6},
+            {id: 7},
+            {id: 8},
+        ]
 
-        const elements = photos.map(d => <div className="item"><img src={this.props.firstPhoto} onClick={this.onImageClick}
-            className={this.props.currentPhotoId === d.id? "photo show": "photo"}/></div>)
+        const elements = photos.map(d => <div className="item">
+            <img src={this.props.firstPhoto}
+                 onClick={this.onImageClick}
+                 className={this.props.currentPhotoId === d.id ? "photo show" : "photo"}/>
+        </div>)
 
         return (
             <div className="App">
-                {/*<span>{this.props.timeOut}</span>*/}
                 <audio ref={this.audioRef} src={voice}></audio>
                 <div className="navigation">
                     <button onClick={this.onStartEvents}>Start</button>
                     <button onClick={this.onStopEvents}>Stop</button>
                 </div>
-
-                <div className={this.props.disableState === true ? "disable":"wrapper"}>
+                <div className={this.props.disableState === true ? "disable" : "wrapper"}>
                     {elements}
                 </div>
                 <div className="counter">
                     {this.props.counter}
                 </div>
             </div>
-        );
+        )
     }
 }
+
 const mapStateToProps = (state) => {
     return {
         currentPhotoId: state.currentPhotoId,
         counter: state.counter,
         firstPhoto: state.firstPhoto,
-        disableState: state. disableState,
+        disableState: state.disableState,
         animation: state.animation,
         timeOut: state.timeOut
     }
@@ -109,16 +103,12 @@ const mapDispatchToProps = (dispatch) => {
             dispatch(startAnimation())
         },
         stopAnimation: () => {
-
             dispatch(stopAnimation())
         },
         stopEvents: () => {
             dispatch(stopEvents())
         },
-        reduceTime: () => {
-            dispatch(reduceTime())
-        }
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,5 @@
 import photo from "../images/dog.jpg"
+import photo_2 from "../images/dog_2.png"
 
 export const IMAGE_CLICK = "IMAGE_CLICK"
 export const RANDOM_IMAGE = "RANDOM_IMAGE"
@@ -6,7 +7,7 @@ export const START_EVENTS = "START_EVENTS"
 export const START_ANIMATION = "START_ANIMATION"
 export const STOP_ANIMATION = "STOP_ANIMATION"
 export const STOP_EVENTS = "STOP_EVENTS"
-export const REDUCE_TIME = "REDUCE_TIME"
+export const DEAD_DOG = "DEAD_DOG"
 
 // функция, которую если вызвать, вернёт нам случайное число от 0 до 8
 function getRandomImageIndex() {
@@ -18,24 +19,27 @@ const initialState = {
     currentPhotoId: 1,
     counter: 1,
     firstPhoto: photo,
+    deadPhoto: photo_2,
+    thirtPhoto: photo,
     disableState: true,
     animation: false,
     timeOut: 5,
-
 }
 
 export const reducer = (state=initialState, action) => {
     switch (action.type) {
         case IMAGE_CLICK:
-
             return {
                 ...state,
-                counter: state.counter +1
+                firstPhoto: state.deadPhoto,
+                counter: state.counter +1,
             }
         case RANDOM_IMAGE:
             return {
                 ...state,
+                // deadPhoto: state.firstPhoto,
                 currentPhotoId: getRandomImageIndex(),
+
             }
             case START_EVENTS:
                 return {
@@ -57,16 +61,19 @@ export const reducer = (state=initialState, action) => {
                 ...state,
                 disableState: true
             }
-        case REDUCE_TIME:
+        // case REDUCE_TIME:
+        //     return {
+        //         ...state,
+        //         timeOut: state.timeOut - 1
+        //     }
+        case DEAD_DOG:
             return {
                 ...state,
-                timeOut: state.timeOut - 1
+                deadPhoto: state.firstPhoto
             }
-
     }
     return state
 }
-
 
 export const imageClick = () => ({type: IMAGE_CLICK})
 export const randomImage = () => ({type: RANDOM_IMAGE})
@@ -74,5 +81,6 @@ export const startEvents = () => ({type: START_EVENTS})
 export const startAnimation = () => ({type: START_ANIMATION})
 export const stopAnimation = () => ({type: STOP_ANIMATION})
 export const stopEvents = () => ({type: STOP_EVENTS})
-export const reduceTime = () => ({type: REDUCE_TIME})
+// export const reduceTime = () => ({type: REDUCE_TIME})
+export const deadDog = () => ({type: DEAD_DOG})
 
